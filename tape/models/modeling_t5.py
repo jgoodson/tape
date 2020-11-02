22fbf4eac50c1e1a076156cf798f81e708a0fffe
Jonathan Goodson <jgoodson@umd.edu>
2020-11-02 15:22:47 -0500

    remove unnecessary token type embeddings

--- tape/models/modeling_t5.py
+++ tape/models/modeling_t5.py
@@ -41,21 +41,14 @@ class ProteinT5Embeddings(nn.Module):
         super().__init__()
         self.word_embeddings = nn.Embedding(
             config.vocab_size, config.hidden_size, padding_idx=0)
-        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
 
         # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be
         # able to load any TensorFlow checkpoint file
         self.LayerNorm = LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-    def forward(self, input_ids, token_type_ids=None):
-        if token_type_ids is None:
-            token_type_ids = torch.zeros_like(input_ids)
-
-        words_embeddings = self.word_embeddings(input_ids)
-        token_type_embeddings = self.token_type_embeddings(token_type_ids)
-
-        embeddings = words_embeddings + token_type_embeddings
+    def forward(self, input_ids):
+        embeddings = self.word_embeddings(input_ids)
         embeddings = self.LayerNorm(embeddings)
         embeddings = self.dropout(embeddings)
         return embeddings
